e41b73f161860158651d6ffd4c2d58bb80a8df64
Isharai1708 <X00205824@myTUDublin.ie>
2023-12-20 01:55:53 +0000

    updated code

--- Models/BPCategory.cs
+++
@@ -1,9 +0,0 @@
-namespace BPCalculator { 
-    public enum BPCategory 
-    { 
-        [System.ComponentModel.DataAnnotations.Display(Name="Low Blood Pressure")] Low, 
-        [System.ComponentModel.DataAnnotations.Display(Name="Ideal Blood Pressure")] Ideal, 
-        [System.ComponentModel.DataAnnotations.Display(Name="Pre-High Blood Pressure")] PreHigh, 
-        [System.ComponentModel.DataAnnotations.Display(Name="High Blood Pressure")] High 
-    } 
-} 
--- Models/BloodPressure.cs
+++ Models/BloodPressure.cs
@@ -8,9 +8,9 @@ namespace BPCalculator
     public enum BPCategory
     {
         [Display(Name="Low Blood Pressure")] Low,
-        [Display(Name="Ideal Blood Pressure")]  Ideal,
+        [Display(Name="Ideal Blood Pressure")] Ideal,
         [Display(Name="Pre-High Blood Pressure")] PreHigh,
-        [Display(Name ="High Blood Pressure")]  High
+        [Display(Name="High Blood Pressure")] High
     };
 
     public class BloodPressure
@@ -21,32 +21,34 @@ namespace BPCalculator
         public const int DiastolicMax = 100;
 
         [Range(SystolicMin, SystolicMax, ErrorMessage = "Invalid Systolic Value")]
-        public int Systolic { get; set; }                       // mmHG
+        public int Systolic { get; set; } // mmHG
 
         [Range(DiastolicMin, DiastolicMax, ErrorMessage = "Invalid Diastolic Value")]
-        public int Diastolic { get; set; }                      // mmHG
+        public int Diastolic { get; set; } // mmHG
 
         // calculate BP category
         public BPCategory Category
-{
-    get
-    {
-        // According to the chart provided
-        if (Systolic >= 140 || Diastolic >= 90)
         {
-            return BPCategory.High;
-        }
-        else if (Systolic >= 120 || Diastolic >= 80)
-        {
-            return BPCategory.PreHigh;
-        }
-        else if (Systolic >= 90 || Diastolic >= 60)
-        {
-            return BPCategory.Ideal;
-        }
-        else
-        {
-            return BPCategory.Low;
-        }
-    }
+            get
+            {
+                // According to the chart provided
+                if (Systolic >= 140 || Diastolic >= 90)
+                {
+                    return BPCategory.High;
+                }
+                else if (Systolic >= 120 || Diastolic >= 80)
+                {
+                    return BPCategory.PreHigh;
+                }
+                else if (Systolic >= 90 || Diastolic >= 60)
+                {
+                    return BPCategory.Ideal;
+                }
+                else
+                {
+                    return BPCategory.Low;
+                }
+            }
+        } // This is the missing closing brace for the getter
+    } // This is the missing closing brace for the class
 }
