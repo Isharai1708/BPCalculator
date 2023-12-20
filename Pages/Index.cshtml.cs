7bfe2409725e14a1a1939d2f5fdd8cf650d63aaa
Isharai1708 <X00205824@myTUDublin.ie>
2023-12-20 01:15:24 +0000

    Added razor pages

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BPCalculator.Pages;
+// page model
 
-public class IndexModel : PageModel
+namespace BPCalculator.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class BloodPressureModel : PageModel
     {
-        _logger = logger;
-    }
+        [BindProperty]                              // bound on POST
+        public BloodPressure BP { get; set; }
 
-    public void OnGet()
-    {
+        // setup initial data
+        public void OnGet()
+        {
+            BP = new BloodPressure() { Systolic = 100, Diastolic = 60 };
+        }
 
+        // POST, validate
+        public IActionResult OnPost()
+        {
+            // extra validation
+            if (!(BP.Systolic > BP.Diastolic))
+            {
+                ModelState.AddModelError("", "Systolic must be greater than Diastolic");
+            }
+            return Page();
+        }
     }
 }
